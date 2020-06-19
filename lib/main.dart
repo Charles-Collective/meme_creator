b1a3f8bc449c53c75629b5c16fdb7978c5ccd96a
sjlacey <seanjlacey@outlook.com>
2020-06-19 15:08:03 +0100

    Fixed image 'File' error by changing 'dart:html' to 'dart:io'. Added arguemnts to getImage for button onPressed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:html';
+import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -12,13 +12,13 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   File _image;
 
-  Future getImage() async {
-    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+  getImage(ImageSource source, BuildContext context) async {
+    var picture = await ImagePicker.pickImage(source: source);
 
-    setState(() {
-      _image = image;
+    this.setState(() {
+      _image = picture;
     });
-  } 
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
             : Image.file(_image),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: getImage,
+        onPressed: getImage(ImageSource.gallery, context),
         tooltip: 'Pick Image',
         child: Icon(Icons.add_a_photo),
       ),
