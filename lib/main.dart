8336d30881c92b5420af3375032f541930b7c234
f1nbar <finbarday@gmail.com>
2020-06-22 17:19:45 +0100

    fixed compile error by wrapping Scaffold in MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 
-void main() {
-  runApp(new MyHomePage());
-}
+
+void main() => runApp(new MyHomePage());
 
 class ImageCapture extends StatefulWidget {
   @override
@@ -42,53 +41,55 @@ class _ImageCaptureState extends State<ImageCapture> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomAppBar(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children:<Widget>[
-            new Text("\n\n\n\n\n\n\n\n\n\n\n"),
-            new Text("Upload a photo to edit", style: TextStyle(fontSize: 28),),
-            new Text("\n"),
-            Row(
-              children: <Widget>[ 
-                new Text("                                  "),
-                IconButton(
-                  icon: Icon(Icons.photo_library, size: 50,),
-                  onPressed:() {
-                    getImage(ImageSource.gallery, context);
-                  }
-                ),
-                new Text("          "),
-                IconButton(
-                  icon: Icon(Icons.photo_camera, size: 50),
-                  onPressed: () {
-                    getImage(ImageSource.camera, context);
-                  }
-                ),
-              ],
-            ),
+    return MaterialApp(
+      home: Scaffold(
+        bottomNavigationBar: BottomAppBar(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children:<Widget>[
+              new Text("\n\n\n\n\n\n\n\n\n\n\n"),
+              new Text("Upload a photo to edit", style: TextStyle(fontSize: 28),),
+              new Text("\n"),
+              Row(
+                children: <Widget>[
+                  new Text("                                  "),
+                  IconButton(
+                    icon: Icon(Icons.photo_library, size: 50,),
+                    onPressed:() {
+                      getImage(ImageSource.gallery, context);
+                    }
+                  ),
+                  new Text("          "),
+                  IconButton(
+                    icon: Icon(Icons.photo_camera, size: 50),
+                    onPressed: () {
+                      getImage(ImageSource.camera, context);
+                    }
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        body: ListView(
+          children: <Widget>[
+            if(_image != null) ...{
+              Image.file(_image, width: 400, height: 400,),
+              Row(
+                children: <Widget>[
+                  FlatButton(
+                    child:Icon(Icons.crop),
+                    onPressed: _cropImage,
+                  ),
+                  FlatButton(
+                    child: Icon(Icons.refresh),
+                    onPressed: _clear,
+                  ),
+                ],
+              ),
+            }
           ],
         ),
-      ),
-      body: ListView(
-        children: <Widget>[
-          if(_image != null) ...{
-            Image.file(_image, width: 400, height: 400,),
-            Row(
-              children: <Widget>[
-                FlatButton(
-                  child:Icon(Icons.crop),
-                  onPressed: _cropImage,
-                ),
-                FlatButton(
-                  child: Icon(Icons.refresh),
-                  onPressed: _clear,
-                ),
-              ],
-            ),
-          }
-        ],
       ),
     );
   }
