e0ef1385afcc39cbc4676036743fcd6c5a47b8d4
f1nbar <finbarday@gmail.com>
2020-07-21 12:29:33 +0100

    updated image_cropper package in yaml and added an image crop method, currently crashes

--- lib/imageedit.dart
+++ lib/imageedit.dart
@@ -1,8 +1,12 @@
+import 'dart:ui';
+import 'dart:async';
+import 'dart:io';
 import 'package:charlesmemeeditor/util/hexcolor.dart';
 import 'package:charlesmemeeditor/imagepick.dart';
 import 'package:charlesmemeeditor/imageedit.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'globals.dart' as global;
 
 class Edit extends StatefulWidget {
@@ -34,7 +38,7 @@ class  _EditState extends State<Edit> {
                       padding: EdgeInsets.only(top: height/10),
                       alignment: FractionalOffset.bottomCenter,
                       icon: Icon(Icons.crop),
-                      //onPressed: _cropImage,
+                      onPressed: _cropImage,
                     ),
                   ),
                   Expanded(
@@ -61,4 +65,40 @@ class  _EditState extends State<Edit> {
       )
     );
   }
-}
+
+  Future<Null> _cropImage() async {
+    File croppedFile = await ImageCropper.cropImage(
+        sourcePath: global.image.path,
+        aspectRatioPresets: Platform.isAndroid
+            ? [
+          CropAspectRatioPreset.square,
+          CropAspectRatioPreset.ratio3x2,
+          CropAspectRatioPreset.original,
+          CropAspectRatioPreset.ratio4x3,
+          CropAspectRatioPreset.ratio16x9
+        ]
+            : [
+          CropAspectRatioPreset.original,
+          CropAspectRatioPreset.square,
+          CropAspectRatioPreset.ratio3x2,
+          CropAspectRatioPreset.ratio4x3,
+          CropAspectRatioPreset.ratio5x3,
+          CropAspectRatioPreset.ratio5x4,
+          CropAspectRatioPreset.ratio7x5,
+          CropAspectRatioPreset.ratio16x9
+        ],
+        androidUiSettings: AndroidUiSettings(
+            toolbarTitle: 'Cropper',
+            toolbarColor: Colors.deepOrange,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false),
+        iosUiSettings: IOSUiSettings(
+          title: 'Cropper',
+        ));
+    if (croppedFile != null) {
+      debugPrint("null");
+      };
+    }
+  }
+
